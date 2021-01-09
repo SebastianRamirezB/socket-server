12b041b17c1325f12d84488e32d7ff8356aa5bd0
Sebastian Ramirez <sebastianramirez748@gmail.com>
2021-01-09 11:29:33 -0500

    Socket Server index public modificado

--- models/server.js
+++ models/server.js
@@ -2,7 +2,8 @@ const express  = require('express');
 const http     = require('http');
 const socketio = require('socket.io');
 const path     = require('path');
-const Sockets = require('./sockets');
+const Sockets  = require('./sockets');
+const cors     = require('cors');
 
 class server {
    
@@ -21,6 +22,9 @@ class server {
     middlewares() {
         //Desplegar el directorio público
         this.app.use(express.static( path.resolve(__dirname, '../public') ));
+
+        //Cors 
+        this.app.use(cors());
     }
 
     configurarSockets(){
